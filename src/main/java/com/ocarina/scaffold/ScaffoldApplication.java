ded1cd5d8e64c50de778f5024d924a0b6ead81b9
Yicheng <yichengzhang023@gmail.com>
2024-07-18 22:18:30 +0800

    Initial commit

---
+++ src/main/java/com/ocarina/scaffold/ScaffoldApplication.java
@@ -0,0 +1,16 @@
+package com.ocarina.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yicheng
+ */
+@SpringBootApplication
+public class ScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocarina/scaffold/ScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocarina.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
